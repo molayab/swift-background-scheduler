13221213f737ecb0803a5ea4d70a1c58aeac4d34
Mateo Olaya Bernal <pub.mateo.olaya@gmail.com>
2026-01-29 20:56:45 -0500

    Initial commit.

---
+++ BackgroundItTests/BackgroundItTests.swift
@@ -0,0 +1,16 @@
+//
+//  BackgroundItTests.swift
+//  BackgroundItTests
+//
+//  Created by Mateo Olaya on 1/28/26.
+//
+
+import Testing
+
+struct BackgroundItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
